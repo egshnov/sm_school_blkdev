e41579fdd6fab9a1215b85ec95f050e3fa70e43f
shalasheg <shalasheg@gmail.com>
2024-07-11 02:32:13 +0300

    HW1 solution

---
+++ Kbuild
@@ -0,0 +1,14 @@
+ccflags-y := 	-Wall					\
+		-Wextra					\
+		-Wno-missing-field-initializers		\
+		-Wno-unused-parameter			\
+		-Wformat				\
+		-O2					\
+		-std=gnu18				\
+		-g					\
+		-Werror=format-security			\
+		-Werror=implicit-function-declaration	
+
+blkm-y := main.o
+
+obj-m := blkm.o
--- main.c
+++ main.c
@@ -1,80 +1,114 @@
 // SPDX-License-Identifier: GPL-2.0-only
 /*
- * some information here
+ * SPBU MM summer school 2024 HW1
  */
 
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/blkdev.h>
-static char *blk_name;
 
-static int __init blk_init(void)
+static char *last_bdev_path;
+struct block_device *bdev; // TODO: some kind of dynamic list?
+fmode_t mode = FMODE_READ | FMODE_WRITE;
+
+static int __init blkm_init(void)
 {
-	pr_info("blkm init\n");
-
-	return 0;
+    pr_info("blkm init\n");
+    return 0;
 }
 
-static void __exit blk_exit(void)
+static void __exit blkm_exit(void)
 {
-	kfree(blk_name);
+    if (last_bdev_path)
+    {
+        blkdev_put(bdev, mode);
+    }
 
-	pr_info("blkm exit\n");
+    kfree(last_bdev_path);
+    pr_info("blkm exit\n");
 }
 
-static int blkm_name_set(const char *arg, const struct kernel_param *kp)
+static int blkm_pipe_add(const char *arg, const struct kernel_param *kp)
 {
-	ssize_t len = strlen(arg) + 1;
+    ssize_t len = strlen(arg) + 1;
+    if (last_bdev_path)
+    {
+        pr_err("Another device is already opened, please remove previous one before opening new one.\n");
+        return -EBUSY;
+    }
 
-	if (blk_name)
-	{
-		kfree(blk_name);
-		blk_name = NULL;
-	}
+    last_bdev_path = kzalloc(sizeof(char) * len, GFP_KERNEL);
 
-	blk_name = kzalloc(sizeof(char) * len, GFP_KERNEL);
-	if (!blk_name)
-		return -ENOMEM;
-	strcpy(blk_name, arg);
+    if (!last_bdev_path)
+        return -ENOMEM;
 
-	return 0;
+    strcpy(last_bdev_path, arg);
+    pr_info("recieved %s\n", last_bdev_path);
+    bdev = blkdev_get_by_path(last_bdev_path, mode, THIS_MODULE);
+
+    if (IS_ERR(bdev))
+    {
+        pr_err("Opening device '%s' failed, err: %d\n", last_bdev_path, PTR_ERR(bdev));
+        goto free_path;
+    }
+    pr_info("opened %s\n", last_bdev_path);
+    return 0;
+
+free_path:
+    kfree(last_bdev_path);
+    last_bdev_path = NULL;
+    return -ENODEV;
 }
 
-// redundant?
-static int blkm_name_get(char *buf, const struct kernel_param *kp)
+static int blkm_pipe_get_name(char *buf, const struct kernel_param *kp)
 {
-	ssize_t len;
+    ssize_t len;
 
-	if (!blk_name)
-		return -EINVAL;
-	len = strlen(blk_name);
+    if (!last_bdev_path)
+        return -EINVAL;
+    len = strlen(last_bdev_path);
 
-	strcpy(buf, blk_name);
+    strcpy(buf, last_bdev_path);
+    strcat(buf, "\n\0"); // TODO: redundant ?
 
-	return len;
+    return len;
 }
 
 static const struct kernel_param_ops blkm_name_ops = {
-	.set = blkm_name_set,
-	.get = blkm_name_get, // redunanat???
+    .set = blkm_pipe_add,
+    .get = blkm_pipe_get_name,
 };
 
+static int blkm_pipe_rm(const char *arg, const struct kernel_param *kp)
+{
+    if (!last_bdev_path)
+    {
+        pr_err("No opened devices\n");
+        return -ENODEV;
+    }
+
+    blkdev_put(bdev, mode);
+    kfree(last_bdev_path);
+    last_bdev_path = NULL;
+    pr_info("device removed\n");
+    return 0;
+}
 static const struct kernel_param_ops blkm_rm = {
-	.set = hwm_say_hello,
-	.get = NULL,
+    .set = blkm_pipe_rm,
+    .get = NULL,
 };
 
-//операция нахождения устройства по имени и его открытия
-MODULE_PARM_DESC(device_name, "Device name");
-module_param_cb(device_name, &blkm_name_ops, NULL, S_IRUGO | S_IWUSR);
+// операция нахождения устройства по имени и его открытия
+MODULE_PARM_DESC(device_pipe, "Device pipe");
+module_param_cb(device_pipe, &blkm_name_ops, NULL, S_IRUGO | S_IWUSR);
 
-//удаление устройства
+// удаление устройства
 MODULE_PARM_DESC(rm_device, "Remove device");
-module_param_cb(rm_device, hwm_hello_times, NULL, S_IRUGO | S_IWUSR);
+module_param_cb(rm_device, &blkm_rm, NULL, S_IWUSR);
 
 module_init(blkm_init);
 module_exit(blkm_exit);
 
-MODULE_AUTHOR("Egor Shalashnov <shalalsheg@gmail.com>");
+MODULE_AUTHOR("Egor Shalashnov <shalasheg@gmail.com>");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("blkm opening module");
